0787f97761bd86b9cafdd81da28a17fd8205b4db
Rakesh Kushwaha <rakesh.kushwaha@plantix.net>
2024-12-01 23:16:15 +0530

    adding flowers list in seperate file

--- app/src/main/java/com/composetutorail/data/DataSource.kt
+++ app/src/main/java/com/composetutorail/data/DataSource.kt
@@ -1,6 +1,7 @@
 package com.composetutorail.data
 
 import com.composetutorail.model.CardData
+import com.composetutorail.model.Flower
 import com.composetutorial.R
 
 /*
@@ -22,6 +23,21 @@ class DataSource {
             CardData("JJJJJJ", R.drawable.ic_launcher_background),
             CardData("KKKKKKKK", R.drawable.ic_launcher_background),
             CardData("LLLLLL", R.drawable.ic_launcher_background),
+        )
+    }
+
+    fun getFlowerDetails(): List<Flower> {
+        return mutableListOf(
+            Flower("aaa", 344, R.drawable.img_1),
+            Flower("fff", 234, R.drawable.img_2),
+            Flower("fff", 344, R.drawable.img_3),
+            Flower("dsds", 878, R.drawable.img_3),
+            Flower("vff", 344, R.drawable.img_2),
+            Flower("aaa", 344, R.drawable.img_1),
+            Flower("aaa", 344, R.drawable.img_1),
+            Flower("aaa", 344, R.drawable.img_2),
+            Flower("aaa", 344, R.drawable.img_3),
+            Flower("bggb", 354, R.drawable.img_1)
         )
     }
 }
