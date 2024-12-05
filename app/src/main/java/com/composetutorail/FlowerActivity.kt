b45665b140cb4d17e5ef4eddd84408eb46beb904
Rakesh Kushwaha <rakesh.kushwaha@plantix.net>
2024-12-05 18:14:39 +0530

    added appbar menu

--- app/src/main/java/com/composetutorail/FlowerActivity.kt
+++ app/src/main/java/com/composetutorail/FlowerActivity.kt
@@ -1,5 +1,6 @@
 package com.composetutorail
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,7 +8,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -19,11 +19,19 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItemDefaults.contentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,23 +49,42 @@ import com.composetutorail.ui.theme.ComposeDemoTheme
 class FlowerActivity : ComponentActivity() {
 
 
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeDemoTheme {
-                Surface(
-                    modifier = Modifier.verticalScroll(rememberScrollState())
-                ) {
-                    PopularFlowersList()
-                }
-            }
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        colors = TopAppBarDefaults.smallTopAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.secondary,
+                            titleContentColor = Color.White,
+                        ),
+                        title = {
+                            Text(text = "ComposeDemo")
+                        },
+//                        navigationIcon = {
+//                            IconButton(onClick = { }) {
+//                                Icon(Icons.Filled.Menu, "")
+//                            }
+//                        },
+                    )
+                }, content = {
+                    Surface(
+                        modifier = Modifier.verticalScroll(rememberScrollState()),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        MainScreen()
+                    }
+                })
         }
     }
 
     @Composable
-    private fun PopularFlowersList() {
+    private fun MainScreen() {
         Column {
-            Surface(modifier = Modifier.padding(10.dp)) {
+            Surface(modifier = Modifier.padding(10.dp, 80.dp, 10.dp, 10.dp)) {
                 Text(
                     text = "Popular Flowers",
                     style = TextStyle(
@@ -91,7 +118,7 @@ class FlowerActivity : ComponentActivity() {
             }
             Surface(modifier = Modifier.padding(10.dp)) {
                 Text(
-                    text = "Flowers",
+                    text = "Testing",
                     style = TextStyle(
                         color = Color.Black,
                         fontSize = 24.sp,
@@ -199,7 +226,7 @@ class FlowerActivity : ComponentActivity() {
     @Composable
     fun FlowersPreview() {
         ComposeDemoTheme {
-            PopularFlowersList()
+            MainScreen()
         }
     }
 }
